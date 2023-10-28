4f110b93f337706380c3aa99ef57f6e3f5dfba6e
yao johanna <yaojohanna2003@gmail.com>
2023-10-28 11:32:43 +0000

    first commit

---
+++ assets/javascript/afficherLivre.js
@@ -0,0 +1,22 @@
+const session = localStorage.sessionLibrairie;
+
+
+fetch("https:/localhost:4020/api/livre/getAll")
+  .then((res) => res.json())
+  .then((succes) => {
+    console.log(succes);
+    const content = document.querySelector("#content");
+    console.log(content);
+    if (succes.data.length) {
+      succes.data.forEach((element) => {
+        content.innerHTML += `<div class="bix">
+            <div class="images"><img src="${element.image}" class="imgs" alt=""></div>
+            <div class="texteo">
+              <p class="lili">${element.titre}</p>
+              <p class="nono">${element.auteur}</p>
+            </div>
+            
+          </div>`;
+      });
+    }
+});
